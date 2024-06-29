3904ca257961b2c65397706872d1451da94cb9e1
Miho Funayama <funamiho1011@gmail.com>
2024-06-29 22:00:55 +0900

    search by name function working

--- script.js
+++ script.js
@@ -4,10 +4,41 @@ function dropdown() {
 
 const countriesContainer = document.getElementById("countries-container");
 
-const url = "https://restcountries.com/v3.1/independent?status=true"
+const baseUrl = "https://restcountries.com/v3.1/independent?status=true"
 
+function searchByWord() {
+  console.log("search by word working");
+  const nameInput = document.getElementById("search-by-name").value
+  const nameUrl = `https://restcountries.com/v3.1/name/${nameInput}`
+
+  fetch(nameUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+    countriesContainer.innerHTML = "";
+
+    data.forEach(country => {
+      const cardHTML = `
+      <div class="country-box">
+      <img src=${country.flags.png} alt="flag-img">
+      <div class="country-detail">
+      <p>${country.name.common}</p>
+      <p>Population: ${country.population}</p>
+      <p>Region: ${country.region}</p>
+      <p>Capital: ${country.capital}</p>
+      </div>
+      </div>
+      `;
+
+      countriesContainer.innerHTML += cardHTML;
+    })
+  })
+}
+
+// display all countries
 document.addEventListener("DOMContentLoaded", () => {
-  fetch(url)
+  fetch(baseUrl)
   .then((response) => response.json())
   .then((data) => {
     console.log(data)
@@ -30,3 +61,5 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 )}
 )
+
+// display the result that matches input string
