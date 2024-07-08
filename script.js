a16c733d5291fab2e292ca3052468093cf02b8e8
Miho Funayama <funamiho1011@gmail.com>
2024-07-08 23:56:29 +0900

    Needs to dentify issue with nativeName that still needs resolution

--- script.js
+++ script.js
@@ -13,37 +13,39 @@ const countriesContainer = document.getElementById("countries-container");
 
 function searchByWord() {
   console.log("search by word working");
-  const searchInput = document.getElementById("search-by-name").value.toLowerCase();
+  const searchInput = document
+    .getElementById("search-by-name")
+    .value.toLowerCase();
   fetchCountries(searchInput, null);
 }
 
 function searchByRegion(region) {
-  fetchCountries(null, region)
+  fetchCountries(null, region);
 }
 
 function fetchCountries(query, region) {
   let apiUrl;
   if (query) {
-    apiUrl = `https://restcountries.com/v3.1/name/${query}`
+    apiUrl = `https://restcountries.com/v3.1/name/${query}`;
   } else if (region) {
-    apiUrl = `https://restcountries.com/v3.1/region/${region}`
+    apiUrl = `https://restcountries.com/v3.1/region/${region}`;
   } else {
-    apiUrl = "https://restcountries.com/v3.1/independent?status=true"
+    apiUrl = "https://restcountries.com/v3.1/independent?status=true";
   }
   fetch(apiUrl)
-.then((response) => response.json())
-.then((data) => {
-  displayCountries(data);
-  console.log(data, "fetched data");
-})
-.catch((error) => console.error('Error fetching countries:', error));
+    .then((response) => response.json())
+    .then((data) => {
+      displayCountries(data);
+      console.log(data, "fetched data");
+    })
+    .catch((error) => console.error("Error fetching countries:", error));
 }
 
-const baseUrl = "https://restcountries.com/v3.1/independent?status=true"
+const baseUrl = "https://restcountries.com/v3.1/independent?status=true";
 
 function displayCountries(data) {
   countriesContainer.innerHTML = "";
-  data.forEach(country => {
+  data.forEach((country) => {
     const cardHTML = `
     <div id="country-box" class="country-box" data-country="${country.name.common}">
     <img src=${country.flags.png} alt="flag-img">
@@ -57,69 +59,100 @@ function displayCountries(data) {
     `;
 
     countriesContainer.innerHTML += cardHTML;
-  })
-
-
+  });
 
   // as data-country attribute is generated dynamically, the following event should be inside displayCountries function.
   const countryBoxes = document.querySelectorAll(".country-box");
-  countryBoxes.forEach(box => {
+  countryBoxes.forEach((box) => {
     box.addEventListener("click", () => {
-      const countryName = box.getAttribute('data-country');
+      const countryName = box.getAttribute("data-country");
       console.log(countryName, "country name");
-      window.location.href = `country-detail.html?country=${encodeURIComponent(countryName)}`
+      window.location.href = `country-detail.html?country=${encodeURIComponent(
+        countryName
+      )}`;
       displayCountryDetail(country);
-  })
-})
+    });
+  });
 }
 
 // home page loading
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Page loaded");
   fetch(baseUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
-    displayCountries(data);
-    })
-  }
-)
+      displayCountries(data);
+    });
+});
 
 // countrydetail page loading
 document.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
-  const countryName = urlParams.get('country');
+  const countryName = urlParams.get("country");
   if (countryName) {
     fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-    .then(response => response.json())
-    .then(data => {
-      if (data && data.length > 0) {
-        displayCountryDetail(data[0])
-      }
-    })
-    .catch(error => console.error("Error fetching country details", error));
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data, "country detail data");
+        if (data && data.length > 0) {
+          // displayCountryDetail(data[0])
+          displayCountryDetail(data[0]);
+        } else {
+          console.error("Country data not found or invalid", data);
+        }
+      })
+      .catch((error) => console.error("Error fetching country details", error));
+  } else {
+    console.error("Country name parameter is missing or invalid:", countryName);
   }
-    })
+});
 
-    function displayCountryDetail(country) {
-      console.log("Hello");
-      console.log(country.population);
-      const countryDetailContainer = document.getElementById("country-detail-container");
+function getNativeNames(country) {
+  const nativeNames = country.nativeName;
+  const keys = Object.keys(nativeNames);
 
-      //
-      const nativeNames = Object.keys(country.name.nativeName)
-      .map(key => country.name.nativeName[key].official)
-      .join(', ')
+  // single key
+  if (keys.length === 1) {
+      const key = keys[0];
+      return nativeNames[key];
+  }
 
-      const currencies = Object.keys(country.currencies)
-      .map(key => country.currencies[key].name)
+  // multiple key
+  const result = {};
+  keys.forEach(key => {
+      result[key] = nativeNames[key];
+  });
+  return result;
+}
 
-      const languages = Object.values(country.languages).join(", ")
+function displayCountryDetail(country) {
+  console.log("Hello");
+  console.log(country.population, "country population");
+  const countryDetailContainer = document.getElementById(
+    "country-detail-container"
+  );
 
-      const borderCountries = Object.values(country.borders).join(", ")
+  //
+  //     const nativeNames = Object.values(country.name.nativeName)
+  // .map(n => n.official)
+  // .join(', ');
+  // const nativeNames = Object.keys(country.name.nativeName)
+  // .map(key => country.name.nativeName[key].official)
+  // .join(', ')
 
-        countryDetailContainer.innerHTML = `
+  const nativeNames = getNativeNames(country);
+
+  const currencies = Object.keys(country.currencies).map(
+    (key) => country.currencies[key].name
+  );
+
+  const languages = Object.values(country.languages).join(", ");
+
+  const borderCountries = Object.values(country.borders).join(", ");
+
+  countryDetailContainer.innerHTML = `
         <div class="flex-container">
         <img src=${country.flags.png} alt="flag-img">
         <div class="country-detail">
@@ -138,4 +171,4 @@ document.addEventListener("DOMContentLoaded", () => {
         </div>
         </div>
         `;
-    }
+}
